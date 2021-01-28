b3c343e86fbcab8e7558225efb2377f7cf87e13a
Adam Etzion <adametzion90@gmail.com>
2021-01-27 18:53:53 -0700

    Add iteration 1, node class and test

---
+++ test/node_test.rb
@@ -0,0 +1,17 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/node'
+
+class NodeTest < Minitest::Test
+    
+    def setup
+        @node = Node.new("plop")
+    end
+
+    def test_it_exists_and_has_attributes
+        assert_instance_of Node, @node
+        assert_equal "plop", @node.data
+        assert_nil @node.next_node
+    end
+        
+end
